c5fd3d8b1ebb8f9cc0b7a2e98140a15eefd66fbb
jordan faucher <jordan@MacBook-Pro-de-jordan.local>
2017-09-05 15:40:05 +0200

    Initial Commit

---
+++ testAmmodys/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testAmmodys
+//
+//  Created by jordan faucher on 05/09/2017.
+//  Copyright © 2017 jordan faucher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
